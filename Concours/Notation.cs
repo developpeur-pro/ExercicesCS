57f6af2b692fc0064ad6edd657fa65c8287aad2d
Cyril <cseguenot@gmail.com>
2023-02-25 20:32:52 +0100

    Ajout exo concours - partie 1

---
+++ Concours/Notation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concours
+{
+	internal enum Mentions { E=0, P=10, AB=12, B=14, TB=16 }
+	
+	internal class Notation
+	{
+		static string[] LibellésMentions = { "Echec", "Passable", "Assez bien", "Bien", "Très bien" };
+		
+		public static (Mentions, string) GetMention(double note)
+		{
+			Mentions mention = Mentions.E;
+			string libellé = LibellésMentions[0];
+
+			int cpt = 0;
+			foreach (Mentions m in Enum.GetValues(typeof(Mentions)))
+			{
+				if ((int)m <= note)
+				{
+					mention = m;
+					libellé = LibellésMentions[cpt++];
+				}
+				else
+					break;
+			}
+
+			return (mention, libellé);
+		}
+	}
+}
---
+++ Concours/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Concours
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Charge le fichier des étudiants dans un tableau de tuples
+			string[] lignes = File.ReadAllLines("Etudiants.csv");
+			(string nom, bool étranger, bool boursier, double moyenne)[] étudiants =
+				new (string, bool, bool, double)[lignes.Length];
+
+			for (int l = 1; l < lignes.Length; l++)
+			{
+				string[] infos = lignes[l].Split(';');
+
+				étudiants[l - 1] = (
+					infos[0] + " " + infos[1],
+					infos[2] == "O",
+					infos[3] == "O",
+					double.Parse(infos[4])
+					);
+			}
+
+			// Affiche les étudiants reçus, avec leurs moyennes et mentions
+			Console.WriteLine("Etudiants reçus au concours :\n");
+			const double seuil = 14.8;
+			int cpt = 0;
+			for (int i = 0; i < étudiants.Length; i++)
+			{
+				double moy = étudiants[i].moyenne;
+				if (moy >= seuil)
+				{
+					cpt++;
+					(Mentions mention, string libellé) mention = Notation.GetMention(moy);
+					Console.WriteLine($"{étudiants[i].nom,-20} : {étudiants[i].moyenne,5:N1}  {mention.libellé}");
+				}
+			}
+
+			Console.WriteLine($"\nTotal : {cpt} étudiants reçus sur {étudiants.Length}.");
+
+			Console.ReadKey();
+		}
+	}
+}
