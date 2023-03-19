4cfc7ea65ba4035a413ba187d1ad1abfa98ae7e3
Cyril <cseguenot@gmail.com>
2023-03-19 19:16:08 +0100

    Ajout exo ModèlesDocs

---
+++ ModelesDocs/Document.cs
@@ -0,0 +1,64 @@
+﻿namespace ModelesDocs
+{
+	internal class Document
+	{
+		#region données et constructeur statiques
+		private static readonly List<Document> Modeles;
+
+		static Document()
+		{
+			Modeles = new List<Document>();
+			Modeles.Add(new Document
+			{
+				Titre = "Modèle A",
+				DateCreation = new DateTime(2023, 1, 1),
+				Marges = (2.5, 2.5, 1.5, 1.5),
+			});
+
+			Modeles.Add(new Document
+			{
+				Titre = "Modèle B",
+				DateCreation = new DateTime(2023, 6, 30),
+				Marges = (2, 2, 1, 1),
+			});
+		}
+		#endregion
+
+		#region Propriétés
+		public string Titre { get; set; } = string.Empty;
+		public Personne? Auteur { get; set; }
+		public DateTime DateCreation { get; set; } = DateTime.Now;
+		public (double Haut, double Bas, double Gauche, double Droite) Marges { get; set; }
+		public string PiedDePage =>
+			$"{Auteur?.Prenom} {Auteur?.Nom ?? "Société XYZ"} - {Titre} - créé le : {DateCreation:d}";
+		#endregion
+
+		#region Méthodes publiques
+		public static Document? CreerDepuisModele(string titreModele, Personne? auteur = null)
+		{
+			Document? doc = null;
+
+			// Recherche le modèle ayant le titre souhaité
+			Document? modele = Modeles.Find(m => m.Titre.ToLower() == titreModele.ToLower());
+
+			// Si on a trouvé un modèle, on crée un doc avec les mêmes titre et marge
+			if (modele != null)
+			{
+				doc = new Document
+				{
+					Titre = modele.Titre,
+					Auteur = auteur,
+					Marges = modele.Marges,
+				};
+			}
+			return doc;
+		}
+		#endregion
+	}
+
+	internal class Personne
+	{
+		public required string Nom { get; init; }
+		public required string Prenom { get; init; }
+	}
+}
---
+++ ModelesDocs/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ModelesDocs
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var auteurs = new Personne[3];
+			auteurs[1] = new Personne { Nom = "Durand", Prenom = "Léo" };
+			auteurs[2] = new Personne { Nom = "Ricaud", Prenom = "Léa" };
+
+			var nomModeles = new string[] { "Modèle A", "modèle B", "Modèle C" };
+
+			for (int i = 0; i < auteurs.Length; i++)
+			{
+				Document? doc = Document.CreerDepuisModele(nomModeles[i], auteurs[i]);
+
+				if (doc == null)
+				{
+					Console.WriteLine($"Aucun modèle trouvé avec le titre {nomModeles[i]}\n");
+					continue;
+				}
+
+				Console.WriteLine($"""
+				Document créé :
+				Pied de page : {doc.PiedDePage}
+				Marges : H={doc.Marges.Haut}cm B={doc.Marges.Bas}cm G={doc.Marges.Gauche}cm D={doc.Marges.Droite}cm
+
+				""");
+			}
+		}
+	}
+}
