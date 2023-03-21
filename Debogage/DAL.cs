3316e51729c847268ac3ce7fc94266f54acfa41a
Cyril <cseguenot@gmail.com>
2023-03-21 14:46:08 +0100

    Ajout exercice débogage

---
+++ Debogage/DAL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debogage
+{
+	[Flags] internal enum Statuts { Aucun = 0, Etranger = 1, Boursier = 2, Admis = 4 }
+
+	internal static class DAL
+	{
+		public static readonly int NbAdmis = 50;
+		private static readonly string NOM_FICHIER = "Etudiants.csv";
+
+		/// <summary>
+		/// Charge le fichier des étudiants dans une liste et renvoie cette liste
+		/// </summary>
+		/// <returns>Liste d'étudiants</returns>
+		public static List<Etudiant> ChargerDonnées()
+		{
+			List<Etudiant> étudiants;
+
+			string[] lignes = File.ReadAllLines(NOM_FICHIER);
+
+			for (int l = 0; l < lignes.Length; l++)
+			{
+				string[] infos = lignes[l].Split(';');
+
+				Statuts st = Statuts.Aucun;
+				if (infos[2] == "O") st |= Statuts.Etranger;
+				if (infos[3] == "O") st |= Statuts.Boursier;
+				if (l <= NbAdmis) st |= Statuts.Admis;
+				
+				Etudiant e = new() {
+					Nom = infos[0],
+					Prénom = infos[1],
+					Moyenne = double.Parse(infos[2]),
+					Statut = st
+				};
+
+				étudiants.Add(e);
+			}
+
+			return étudiants;
+		}
+	}
+
+	internal class Etudiant
+	{
+		public required string Nom { get; init; }
+		public required string Prénom { get; init; }
+		public double Moyenne { get; set; }
+		public Statuts Statut { get; set; }
+	}
+}
---
+++ Debogage/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Debogage
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Etudiant> étudiants = DAL.ChargerDonnées();
+			AfficherRésultatsConcours(étudiants);
+			Console.ReadKey();
+			Console.Clear();
+
+			RechercherEtudiantParNom(étudiants, "leduc");
+
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Affiche le texte passé en paramètre avec la couleur spécifiée
+		/// </summary>
+		/// <param name="texte">texte à afficher</param>
+		/// <param name="couleur">couleur de police à utiliser</param>
+		static void AfficherTexte(string texte, ConsoleColor couleur = ConsoleColor.Blue)
+		{
+			ConsoleColor couleurOrigine = Console.ForegroundColor;
+			Console.ForegroundColor = couleur;
+			Console.WriteLine(texte);
+			Console.ForegroundColor = couleurOrigine;
+		}
+
+		// Affiche les résultats du concours (étudiants avec leurs moyennes et mentions)
+		static void AfficherRésultatsConcours(List<Etudiant> étudiants)
+		{
+			AfficherTexte($"Résultats du concours :\n");
+			foreach (Etudiant e in étudiants)
+			{
+				string res = e.Statut.HasFlag(Statuts.Admis) ? "Admis" : string.Empty;
+
+				Console.WriteLine($"{e.Nom,-15} {e.Prénom,-15}: {e.Moyenne,5:N1}  {res}");
+			}
+
+			AfficherTexte($"\n{DAL.NbAdmis} étudiants admis sur {étudiants.Count}", ConsoleColor.DarkGreen);
+		}
+
+		/// <summary>
+		/// Recherche un étudiant par son nom et affiche ses infos
+		/// </summary>
+		/// <param name="étudiants">liste des étudiants</param>
+		/// <param name="recherche">Nom recherché</param>
+		static void RechercherEtudiantParNom(List<Etudiant> étudiants, string recherche)
+		{
+			AfficherTexte($"Recherche de l'étudiant(e) {recherche}:\n");
+
+			var res = étudiants.Find(e => e.Nom.StartsWith(recherche));
+
+			Console.WriteLine($"{res.Nom} {res.Prénom}, moyenne = {res.Moyenne}");
+		}
+	}
+}
