b6bbaddc169e0697773c93527105e1f7033ceb48
Cyril <cseguenot@gmail.com>
2023-05-28 19:02:33 +0200

    Ajout des projets MenuActions et GestionStock

---
+++ GestionStock/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace GestionStock
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Crée un stock avec seuil d'alerte à 50;
+			Stock stock = new();
+			stock.SeuilAlerte = 50m;
+
+			// Définit un gestionnaire pour l'événement
+			stock.AlerteStockBas += (sender, e) =>
+				Console.WriteLine($"Attention, au {e.Item1:d}, il ne reste que {e.Item2} kg en stock !");
+
+			// Charge le fichier et crée les mouvements de stock
+			Console.WriteLine("Création des mouvements de stocks");
+
+			var mvts = File.ReadAllLines("MouvementsStock.csv");
+			DateOnly jour;
+			decimal qté;
+
+			for (int l = 1; l < mvts.Length; l++)
+			{
+				string[] infos = mvts[l].Split(';');
+				jour = DateOnly.Parse($"{infos[0]}/{DateTime.Today.Year}");
+				qté = decimal.Parse(infos[1]);
+
+				try
+				{
+					if (qté < 0)
+						stock.Retirer(jour, -qté);
+					else
+						stock.Ajouter(jour, qté);
+				}
+				catch (ArgumentException)
+				{
+					Console.WriteLine($"Erreur : il y a déjà un mouvement de stock au {jour:d}");
+				}
+			}
+
+			Console.WriteLine($"{mvts.Length - 1} mouvements de stocks créés.");
+			Console.WriteLine();
+
+			// Affiche l'état du stock au 1er jour de chaque mois
+			for (int m = 1; m <= 12; m++)
+			{
+				var date = new DateOnly(DateTime.Today.Year, m, 1);
+				Console.WriteLine($"Etat du stock au {date:d} : {stock.GetEtatStock(date)} kg");
+			}
+		}
+	}
+}
---
+++ MenusActions/Actions.cs
@@ -0,0 +1,15 @@
+﻿namespace MenusActions
+{
+	public class Actions
+	{
+		public static void Action1()
+		{
+			Console.WriteLine("Exécution de l'action 1");
+		}
+
+		public static void Action2()
+		{
+			Console.WriteLine("Exécution de l'action 2");
+		}
+	}
+}
